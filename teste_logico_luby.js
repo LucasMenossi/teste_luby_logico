ceeddc012e7b476a0077054c17ea7bc950d082aa
Lucas Menossi <lucasmenossi03@gmail.com>
2020-09-21 21:10:30 -0300

    Correção

--- teste_logico_luby.js
+++ teste_logico_luby.js
@@ -37,3 +37,78 @@ function fibonacci(num) {
 }
 
 console.log(fibonacci(10));
+
+//4 Crie uma função chamada estatistica que retorne a média aritmética de um array, a moda e
+//a mediana. Deve ser retornado como um objeto.
+//var dataset = [193,123, 44, 9, 44, 801, 1800, 44, 98, 801, 999, 1780]
+
+function estatistica(dataset) {
+  let media,
+    mediana,
+    sum = 0;
+  let moda = [];
+
+  //Media
+  for (let cont = 0; cont <= dataset.length - 1; cont++) {
+    sum = sum + dataset[cont];
+  }
+  media = sum / dataset.length;
+
+  //Moda
+  let freq = 0,
+    freqNum,
+    list = [];
+
+  dataset.forEach(function (num) {
+    let foundNum = list.find(function (ele) {
+      return ele.num == num;
+    });
+
+    if (foundNum) {
+      foundNum.count++;
+      if (foundNum.count > freq) {
+        freqNum = num;
+        freq = foundNum;
+      }
+    } else list.push({ num: num, count: 1 });
+  });
+
+  moda = freqNum;
+
+  //Mediana
+  if (dataset.length % 2 === 0) {
+    const somaMediana =
+      dataset[dataset.length / 2 - 1] + dataset[dataset.length / 2];
+    mediana = somaMediana / 2;
+  } else mediana = dataset[(dataset.length - 1) / 2];
+
+  return {
+    media,
+    moda,
+    mediana,
+  };
+}
+
+var dataset = [193, 123, 44, 9, 44, 801, 1800, 44, 98, 801, 999, 1780];
+console.log(estatistica(dataset));
+
+//5 Você está desenvolvendo um game, e pedem para você criar uma função que retornará um
+//boleano para identificar se ouve colisão entre dois objetos. Considere que são dois
+//quadrados, e você poderá acessar as coordenadas dentro de cada objeto.
+//objeto1 = {x1:1, y1:1, x2:2, y2:2} objeto2 = {x1:10, y1:10, x2:11, y2:11}
+//A sua função deve receber os dois objetos como parametros e comparar quando alguma das
+//coordendas dos objetos se conflitam.
+
+const objeto1 = { x1: 1, y1: 1, x2: 2, y2: 2 };
+const objeto2 = { x1: 10, y1: 10, x2: 11, y2: 11 };
+
+function checkCollision(obj1, obj2) {
+  if (
+    (obj1.x1 === obj2.x1 && obj1.y1 === obj2.y1) ||
+    (obj1.x2 === obj2.x2 && obj1.y2 === obj2.y2)
+  )
+    return "Há colisão";
+  else return "Não há colisão";
+}
+
+console.log(checkCollision(objeto1, objeto2));
